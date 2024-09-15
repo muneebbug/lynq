7a8ded00b28e15b12b59bdede7077ba13c4c5f98
Muneeb <freelancermuneeb@gmail.com>
2024-09-15 08:10:22 +0500

    refactor ♻️: better global middleware code.

--- middleware/0.auth.global.ts
+++ middleware/0.auth.global.ts
@@ -1,20 +1,43 @@
-export default defineNuxtRouteMiddleware((to) => {
+// middleware/auth.ts
+import {
+  publicRoutes,
+  authRoutes,
+  protectedRoutes,
+  checkRoutesPrefix,
+  apiAuthPrefix,
+  DEFAULT_LOGIN_REDIRECT_URL,
+} from '@/lib/routes'
+
+export default defineNuxtRouteMiddleware(async (to) => {
   const { status } = useAuth()
   const isAuthenticated = status.value === 'authenticated'
-  const authDisabledPaths = ['/auth/login', '/']
 
-  // If user is authenticated and trying to access an authDisabledPath, redirect to '/'
-  if (authDisabledPaths.includes(to.path)) {
-    if (isAuthenticated && to.path !== '/') {
-      return navigateTo('/')
-    }
-  }
-  else {
-    // If user is not authenticated and trying to access a protected route, redirect to '/auth/login'
-    if (!isAuthenticated && to.path !== '/auth/login') {
-      return navigateTo('/auth/login')
-    }
+  const isApiAuthRoute = to.path.startsWith(apiAuthPrefix)
+  const isCheckRoute = to.path.startsWith(checkRoutesPrefix)
+  const isProtectedRoute = protectedRoutes.includes(to.path)
+  const isPublicRoute = publicRoutes.includes(to.path)
+  const isAuthRoute = authRoutes.includes(to.path)
+
+  // Skip API routes
+  if (isApiAuthRoute) {
+    return
   }
 
-  return true
+  // Handle Auth Routes
+  if (isAuthRoute) {
+    if (isAuthenticated) {
+      return navigateTo(DEFAULT_LOGIN_REDIRECT_URL)
+    }
+    return
+  }
+
+  // Handle Protected Routes
+  if (isProtectedRoute && !isAuthenticated) {
+    const callbackUrl = to.fullPath
+    return navigateTo(`/auth/login?callbackUrl=${encodeURIComponent(callbackUrl)}`)
+  }
+
+  // Handle Check Routes
+  const slugRoute = to.path.split('/').pop()
+  if (!isPublicRoute && !isProtectedRoute && !isCheckRoute) return
 })
