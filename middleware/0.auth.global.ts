e40ae0b9bdda16cfbbe077ca8d80ed1ded35db72
Muneeb <freelancermuneeb@gmail.com>
2024-09-15 07:56:08 +0500

    fix 🐛: global middleware infinite loop

--- middleware/0.auth.global.ts
+++ middleware/0.auth.global.ts
@@ -2,15 +2,19 @@ export default defineNuxtRouteMiddleware((to) => {
   const { status } = useAuth()
   const isAuthenticated = status.value === 'authenticated'
   const authDisabledPaths = ['/auth/login', '/']
+
+  // If user is authenticated and trying to access an authDisabledPath, redirect to '/'
   if (authDisabledPaths.includes(to.path)) {
-    if (isAuthenticated) {
+    if (isAuthenticated && to.path !== '/') {
       return navigateTo('/')
     }
   }
   else {
-    if (!isAuthenticated) {
+    // If user is not authenticated and trying to access a protected route, redirect to '/auth/login'
+    if (!isAuthenticated && to.path !== '/auth/login') {
       return navigateTo('/auth/login')
     }
   }
+
   return true
 })
